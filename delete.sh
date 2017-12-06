ec319efeed8baf5f1d92d54f738429e2b865d217
pct960 <pct960@gmail.com>
2017-12-06 12:12:32 +0530

    Fix: RabbitMQ logs now mapped correctly
    Fix: Delete script changed to include new directories in
    persistent storage

--- delete.sh
+++ delete.sh
@@ -2,5 +2,6 @@ cd /data/kong && sudo rm -rf *
 cd /data/tomcat && sudo rm -rf *
 cd /data/rabbitmq && sudo rm -rf *
 cd /data/logs/kong && sudo rm -rf *
-cd /data/logs/rabbitmq && sudo rm -rf *
+cd /data/logs/rabbitmq/rmq && sudo rm -rf *
+cd /data/logs/rabbitmq/supervisor && sudo rm -rf *
 cd /data/logs/tomcat && sudo rm -rf *
